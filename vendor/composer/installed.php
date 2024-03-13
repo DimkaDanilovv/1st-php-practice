fd95d710ad8524a1d012e34c9674b4b621833dd3
DimkaDanilovv <DanilovDmitriy769d@gmail.com>
2024-03-13 17:52:37 +0700

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dimkadanilovv/1st-php-practice',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '18fb79bde596472564a26a7ac4d842ee134139e1',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dimkadanilovv/1st-php-practice' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '18fb79bde596472564a26a7ac4d842ee134139e1',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
